93058bf713bba21d92d97d9609c6de5d9a47f2d9
Jez Higgins <jez@jezuk.co.uk>
2017-11-24 16:08:34 +0000

    Hooked up Ethereum.store

--- src/driver/Ethereum.js
+++ src/driver/Ethereum.js
@@ -1,5 +1,6 @@
 import Web3 from 'web3';
 import ArchangelABI from './ethereum/Archangel';
+import { DateTime } from 'luxon';
 
 const ArchangelAddress = '0x40aa476f8ae7105d9094e8293b633273c085a3d5';
 const NullId = '0x0000000000000000000000000000000000000000000000000000000000000000';
@@ -9,9 +10,16 @@ function missingMetaMask() {
   return null;
 } // missingMetaMask
 
+let web3_;
+function web3() {
+  if (web3_)
+    return web3_;
+  web3_ = new Web3(window.web3.currentProvider);
+  return web3_;
+} // web3
+
 function createContract() {
-  const web3 = new Web3(window.web3.currentProvider);
-  const contractClass = web3.eth.contract(ArchangelABI);
+  const contractClass = web3().eth.contract(ArchangelABI);
   return contractClass.at(ArchangelAddress);
 } // archangelContract
 
@@ -31,7 +39,12 @@ function archangelContract() {
 
 class Ethereum {
   async store(id, payload, timestamp) {
-    alert("Ethereum.store not yet implemented");
+    const slug = {
+      id: id,
+      payload: payload,
+      timestamp: timestamp
+    };
+    return this.eth_store(id, slug);
   } // store
 
   async fetch(id) {
@@ -50,6 +63,42 @@ class Ethereum {
   } // fetch
 
   ////////////////////////
+  eth_store(id, slug) {
+    const contract = archangelContract();
+    const slugStr = JSON.stringify(slug);
+
+    return new Promise((resolve, reject) => {
+      const onCommitted = (tx, timeout) => {
+        web3().eth.getTransactionReceipt(tx, (err, result) => {
+          if (err)
+            return reject(err);
+
+          if (result)
+            return resolve({
+              tx: tx,
+              receipt: result
+            });
+
+          const diff = timeout.diff(DateTime.local(), 'seconds').values.seconds;
+          if (diff <= 0)
+            return reject(new Error(`Transaction ${tx} wasn't processed within ${this.txTimeout} seconds.`));
+
+          setTimeout(() => onCommitted(tx, timeout), 1000);
+        });
+      }; // onCommitted
+
+      const timeout = DateTime.local().plus({seconds: 60});
+      contract.store(id, slugStr,
+        { from: web3().eth.accounts[0].toLowerCase() },
+        (err, tx) => {
+          if (err)
+            return reject(err);
+          // transaction(tx);
+          onCommitted(tx, timeout);
+        });
+    })
+  } // eth_store
+
   eth_fetch(id) {
     return this.eth_call_fetch('fetch', id);
   } // eth_fetch
