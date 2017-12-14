4cf86363b0f05cf428e7cfccda501cdb82af21f1
Jez Higgins <jez@jezuk.co.uk>
2017-12-14 15:30:39 +0000

    First tiny bit of source code

---
+++ src/main/groovy/archangeldlt/Archangel.groovy
@@ -0,0 +1,10 @@
+package archangeldlt;
+
+import uk.gov.nationalarchives.droid.profile.ProfileManager
+import uk.gov.nationalarchives.droid.core.interfaces.signature.SignatureManager;
+
+public class Archangel {
+  public static void main(String[] args) {
+    println "Hello Mother"
+  }
+} // class Archangel
