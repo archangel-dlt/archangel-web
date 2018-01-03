fc63ce2a6de69cd1afd5b9c6edc6e678425cf4de
Jez Higgins <jez@jezuk.co.uk>
2018-01-03 12:25:56 +0000

    Start driving Droid to generate CSV output

--- src/main/groovy/archangeldlt/Archangel.groovy
+++ src/main/groovy/archangeldlt/Archangel.groovy
@@ -1,10 +1,34 @@
-package archangeldlt;
+package archangeldlt
 
-import uk.gov.nationalarchives.droid.profile.ProfileManager
-import uk.gov.nationalarchives.droid.core.interfaces.signature.SignatureManager;
+import org.apache.commons.lang.RandomStringUtils
+import uk.gov.nationalarchives.droid.command.DroidCommandLine
 
-public class Archangel {
-  public static void main(String[] args) {
-    println "Hello Mother"
+class Archangel {
+  static void main(String[] args) {
+    if (args.length == 0) {
+      println("Give us a filename then chummy")
+      return
+    }
+
+    DroidCommandLine.systemExit = false
+
+    def profileName = uniqueName()
+    DroidCommandLine.main(["-a", "\"${args[0]}\"", "-A", "-p", "\"${profileName}.droid\""] as String[]);
+    DroidCommandLine.main(["-p", "\"${profileName}.droid\"", "-e", "\"${profileName}.csv\""] as String[]);
+
+    println("Welp, let's see what we've got")
+    System.exit(0)
+  }
+
+  static private String uniqueName() {
+    long millis = System.currentTimeMillis()
+    String datetime = new Date().toGMTString()
+    datetime = datetime.replace(" ", "")
+    datetime = datetime.replace(":", "")
+
+    String rndchars = RandomStringUtils.randomAlphanumeric(16)
+    String filename = rndchars + "_" + datetime + "_" + millis
+
+    return filename
   }
 } // class Archangel
