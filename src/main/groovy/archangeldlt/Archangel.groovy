e1ebee8a90eddd37100bfdeca54dcff502b748bf
Jez Higgins <jez@jezuk.co.uk>
2018-01-10 14:46:24 +0000

    Pulled out CmdLine

--- src/main/groovy/archangeldlt/Archangel.groovy
+++ src/main/groovy/archangeldlt/Archangel.groovy
@@ -1,22 +1,7 @@
 package archangeldlt
 
-import static DroidWrapper.*
-
 class Archangel {
   static void main(String[] args) {
-    if (args.length == 0) {
-      println("Give us a filename then chummy")
-      return
-    }
-
-    setupDroid()
-
-    def csvExport = characterizeFile(args[0])
-    def jsonExport = convertExportToJson(csvExport)
-
-    println '\n\n\n'
-    jsonExport.each { println it }
-
-    System.exit(0)
+    CmdLine.main(args)
   } // main
 } // class Archangel
---
+++ src/main/groovy/archangeldlt/CmdLine.groovy
@@ -0,0 +1,22 @@
+package archangeldlt
+
+import static archangeldlt.DroidWrapper.*
+
+class CmdLine {
+  static void main(String[] args) {
+    if (args.length == 0) {
+      println("Give us a filename then chummy")
+      return
+    }
+
+    setupDroid()
+
+    def csvExport = characterizeFile(args[0])
+    def jsonExport = convertExportToJson(csvExport)
+
+    println '\n\n\n'
+    jsonExport.each { println it }
+
+    System.exit(0)
+  } // main
+} // class Archangel
