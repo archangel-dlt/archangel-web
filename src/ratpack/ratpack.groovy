508742893e277d47c91fec5b93dc18e03de73962
Jez Higgins <jez@jezuk.co.uk>
2018-01-11 10:13:37 +0000

    Simple file upload form

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,9 +1,22 @@
+import static ratpack.groovy.Groovy.htmlBuilder
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
   handlers {
     get {
-      render "Hello World!"
+      render htmlBuilder {
+        html {
+          head {
+            title "Hello"
+          }
+          body {
+            form (action: 'upload', method: 'post', enctype: 'multipart/form-data') {
+              input (type: 'file', name: 'candidate', id: 'candidate') { }
+              input (type: 'submit', value: 'Droid!', name: 'submit') { }
+            }
+          }
+        }
+      }
     }
     get(":name") {
       render "Hello $pathTokens.name!"
