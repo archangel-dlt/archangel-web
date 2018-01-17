0d1b0f32fd86ba64a8142d6917d7d59853116470
Jez Higgins <jez@jezuk.co.uk>
2018-01-17 11:57:12 +0000

    Set filename of uploaded file

--- back-end/src/ratpack/ratpack.groovy
+++ back-end/src/ratpack/ratpack.groovy
@@ -2,10 +2,8 @@ import ratpack.form.Form
 
 import static archangeldlt.DroidWrapper.characterizeFile
 import static archangeldlt.DroidWrapper.convertExportToJson
-import static ratpack.groovy.Groovy.htmlBuilder
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.jackson.Jackson.json
-
 
 ratpack {
   handlers {
@@ -17,7 +15,12 @@ ratpack {
           render "No file uploaded"
           return
         }
-        File.createTempFile("archangel-droid", ".tmp").with { file ->
+
+        File.createTempDir('archangel-droid', 'tmp').with { dir ->
+          println "Created directory ${dir.name}"
+          def file = new File(dir, candidate.fileName)
+          println "Created file ${file.name}"
+
           file.withOutputStream { os ->
             candidate.writeTo(os)
           }
@@ -27,8 +30,10 @@ ratpack {
           file.delete()
 
           render json(jsonExport)
-        }
-      }
+
+          dir.delete()
+        } // File.createTempDir
+      } // upload
     } // post
     files {
       dir "front-end" indexFiles "index.html"
