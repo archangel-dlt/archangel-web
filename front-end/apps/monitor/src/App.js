36d1a2b031768dfd2b604043838a1517a7d48471
Jez Higgins <jez@jezuk.co.uk>
2018-08-03 12:43:41 +0100

    Group related registration and update events together

--- front-end/apps/monitor/src/App.js
+++ front-end/apps/monitor/src/App.js
@@ -3,12 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap/css/bootstrap.css';
 import { ReactEthereum } from '@archangeldlt/web-common';
-import { Puid, prettysize } from '@archangeldlt/web-common';
 import Permissions from './ui/Permissions';
 import Blocknumber from './ui/Blocknumber';
+import EventLog from './ui/Eventlog';
 
 const ethereumDriver = ReactEthereum();
-const maxEvents = 2000;
 
 function Logo() {
   return (
@@ -34,128 +33,6 @@ function Header() {
 } // Header
 
 class Body extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      events: [],
-      groupedEvents: new Map()
-    }
-
-    this.names = {}
-  } // constructor
-
-  componentDidMount() {
-    ethereumDriver.watchEvents(evt => this.event(evt));
-  } // componentDidMount
-
-  event(evt) {
-    if (evt === ethereumDriver.resetEvent) {
-      this.setState({
-        events: [ ],
-        groupedEvents: new Map()
-      });
-      return;
-    }
-
-    const events = this.state.events;
-
-    if(events.unshift(evt) > maxEvents)
-      events.pop();
-
-    const groupedEvents = this.state.groupedEvents;
-    const key = evt.args._key || evt;
-    const eventList = groupedEvents.get(key) || [ ]
-    eventList.push(evt);
-    groupedEvents.set(key, eventList);
-
-    this.setState({
-      events: events,
-      groupedEvents: groupedEvents
-    });
-
-    console.log(this.state.groupedEvents);
-
-
-  } // event
-
-  formatEvent(name, args) {
-    switch(name) {
-      case 'Registration':
-      case 'Update':
-        return this.formatRegistration(args);
-      case 'NoWritePermission':
-        return this.formatNoWrite(args);
-      case 'PermissionGranted':
-        return this.formatPermissionGranted(args);
-      case 'PermissionRemoved':
-        return this.formatPermissionRemoved(args);
-      default:
-        return JSON.stringify(args)
-    }
-  } // formatEvent
-
-  formatNoWrite(args) {
-    return (<div className="col-12">From {args._addr}</div>);
-  } // formatNoWrite
-
-  formatRegistration(args) {
-    const record = JSON.parse(args._payload);
-    return (
-      <div className="col-12 row">
-        <div className="row col-12">
-          <div className="col-8"><strong>{record.name}</strong></div>
-          <div className="col-2"><Puid fmt={record.puid}/></div>
-          <div className="col-2">{ prettysize(record.size, true) }</div>
-        </div>
-        <div className="row col-12">
-          <div className="col-8">{record.sha256_hash}</div>
-          <div className="col-4">Last Modified: {record.last_modified}</div>
-        </div>
-        <div className="row col-12">
-          <div className="col-8">{record.comment}</div>
-          <div className="col-4">Uploaded by <strong>{this.names[args._addr]}</strong> at {record.timestamp} </div>
-        </div>
-        {
-          record.parent_sha256_hash &&
-          <div className="col-12">Parent: <i>{record.parent_sha256_hash}</i></div>
-        }
-      </div>
-    )
-  } // formatRegistration
-
-  formatPermissionGranted(args) {
-    if (args._name === 'contract')
-      this.contractOwner = args._addr;
-
-    this.names[args._addr] = args._name;
-
-    return (<div className="col-12">To <strong>{args._name}</strong>, {args._addr}</div>);
-  } // formatPermissionGranted
-
-  formatPermissionRemoved(args) {
-    return (<div className="col-12">From <strong>{args._name}</strong>, {args._addr}</div>);
-  } // formatPermissionGranted
-
-  formatEvents() {
-    const events = this.state.events;
-
-    return events.map(evt => {
-      return (
-        <div key={evt.transactionHash}>
-          <div className="row">
-            <div className="col-2">Block <a href={`https://rinkeby.etherscan.io/txs?block=${evt.blockNumber}`}>{evt.blockNumber}</a></div>
-            <div className="col-10"><strong>{evt.event}</strong></div>
-          </div>
-          <div className="row">
-            { this.formatEvent(evt.event, evt.args) }
-          </div>
-          <hr/>
-        </div>
-      );
-    });
-  } // events
-
   render() {
     return (
       <React.Fragment>
@@ -165,7 +42,7 @@ class Body extends Component {
 
         <Permissions/>
 
-        { this.formatEvents() }
+        <EventLog/>
       </React.Fragment>
     )
   }
---
+++ front-end/apps/monitor/src/ui/Eventlog.js
@@ -0,0 +1,150 @@
+import React, {Component, Fragment} from 'react';
+import { ReactEthereum } from '@archangeldlt/web-common';
+import { Puid, prettysize } from '@archangeldlt/web-common';
+
+
+const ethereumDriver = ReactEthereum();
+const maxEvents = 2000;
+
+class Eventlog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groupedEvents: new Map()
+    }
+
+    this.names = {}
+  } // constructor
+
+  componentDidMount() {
+    ethereumDriver.watchEvents(evt => this.event(evt));
+  } // componentDidMount
+
+  event(evt) {
+    if (evt === ethereumDriver.resetEvent) {
+      this.setState({
+        events: [ ],
+        groupedEvents: new Map()
+      });
+      return;
+    }
+
+    const groupedEvents = this.state.groupedEvents;
+
+    if(groupedEvents.size > maxEvents)
+      groupedEvents.delete(groupedEvents.keys().next().value);
+
+    const key = evt.args._key || evt;
+    const eventList = groupedEvents.get(key) || [ ]
+    eventList.push(evt);
+    groupedEvents.set(key, eventList);
+
+    this.setState({
+      groupedEvents: groupedEvents
+    });
+  } // event
+
+  formatEvent(name, args) {
+    switch(name) {
+      case 'Registration':
+      case 'Update':
+        return this.formatRegistration(args);
+      case 'NoWritePermission':
+        return this.formatNoWrite(args);
+      case 'PermissionGranted':
+        return this.formatPermissionGranted(args);
+      case 'PermissionRemoved':
+        return this.formatPermissionRemoved(args);
+      default:
+        return JSON.stringify(args)
+    }
+  } // formatEvent
+
+  formatNoWrite(args) {
+    return (<div className="col-12">From {args._addr}</div>);
+  } // formatNoWrite
+
+  formatRegistration(args) {
+    const record = JSON.parse(args._payload);
+    return (
+      <div className="col-12 row">
+        <div className="row col-12">
+          <div className="col-8"><strong>{record.name}</strong></div>
+          <div className="col-2"><Puid fmt={record.puid}/></div>
+          <div className="col-2">{ prettysize(record.size, true) }</div>
+        </div>
+        <div className="row col-12">
+          <div className="col-8">{record.sha256_hash}</div>
+          <div className="col-4">Last Modified: {record.last_modified}</div>
+        </div>
+        <div className="row col-12">
+          <div className="col-8">{record.comment}</div>
+          <div className="col-4">Uploaded by <strong>{this.names[args._addr]}</strong> at {record.timestamp} </div>
+        </div>
+        {
+          record.parent_sha256_hash &&
+          <div className="col-12">Parent: <i>{record.parent_sha256_hash}</i></div>
+        }
+      </div>
+    )
+  } // formatRegistration
+
+  formatPermissionGranted(args) {
+    if (args._name === 'contract')
+      this.contractOwner = args._addr;
+
+    this.names[args._addr] = args._name;
+
+    return (<div className="col-12">To <strong>{args._name}</strong>, {args._addr}</div>);
+  } // formatPermissionGranted
+
+  formatPermissionRemoved(args) {
+    return (<div className="col-12">From <strong>{args._name}</strong>, {args._addr}</div>);
+  } // formatPermissionGranted
+
+  renderEvents(first, rest = []) {
+    return (
+      <div key={first.transactionHash}>
+        <div className="row">
+          <div className="col-2">Block {first.blockNumber}</div>
+          <div className="col-10"><strong>{first.event}</strong></div>
+        </div>
+        <div className="row">
+          { this.formatEvent(first.event, first.args) }
+        </div>
+        <div className="row offset-1 col-10">
+          { rest.map(e => this.renderEvents(e)) }
+        </div>
+      </div>
+    );
+  } // renderEvents
+
+  renderEventGroup(eventGroup) {
+    const lastIndex = eventGroup.length-1;
+    const evt = eventGroup[eventGroup.length-1];
+    const otherEvts = eventGroup.slice(0, lastIndex).reverse();
+
+    return (
+      <div>
+        { this.renderEvents(evt, otherEvts) }
+        <hr/>
+      </div>
+    )
+  } // renderEventGroup
+
+  render() {
+    const events = this.state.groupedEvents;
+
+    const renderedEvents = [];
+
+    for (const group of events.values()) {
+      const renderedGroup = this.renderEventGroup(group);
+      renderedEvents.unshift(renderedGroup);
+    } // for ...
+
+    return renderedEvents;
+  } // render
+} // class Eventlog
+
+export default Eventlog;
