b9f2053ebf180223ed9351c9d5ea519915c8c51a
Jez Higgins <jez@jezuk.co.uk>
2018-08-15 15:20:51 +0100

    Capture SIP data

--- front-end/apps/ui/src/components/upload/SipInfo.js
+++ front-end/apps/ui/src/components/upload/SipInfo.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import { Field } from '@archangeldlt/web-common';
 
-class SipInfo extends Component {
+const citation = 'citation';
+const supplier = 'supplier';
+const creator = 'creator';
+const rights = 'rights';
+const held = 'held';
+
+class SipInfo extends PureComponent {
+  get data() {
+    return {
+      [citation]: this[citation],
+      [supplier]: this[supplier],
+      [creator]: this[creator],
+      [rights]: this[rights],
+      [held]: this[held]
+    };
+  } // data
+
+  update(field, value) {
+    this[field] = value
+  } // update
+
   render() {
-    return (<strong>SIP data here</strong>)
-  }
+    return (
+      <Fragment>
+        <Field title='Citation Reference' length='small' onValue={v => this.update(citation, v)}/>
+        <hr/>
+        <Field title='Supplier' onValue={v => this.update(supplier, v)}/>
+        <Field title='Creator' onValue={v => this.update(creator, v)}/>
+        <hr/>
+        <Field title='Rights statement' onValue={v => this.update(rights, v)}/>
+        <Field title='Held By' onValue={v => this.update(held, v)}/>
+      </Fragment>
+    );
+  } // render
 } // Class SipInfo
 
 export default SipInfo;
