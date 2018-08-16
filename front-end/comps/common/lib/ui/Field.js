b820a818d9a4135148f3cf700f3a9623acb06dea
Jez Higgins <jez@jezuk.co.uk>
2018-08-16 12:38:14 +0100

    added disabled prop

--- front-end/comps/common/lib/ui/Field.js
+++ front-end/comps/common/lib/ui/Field.js
@@ -28,6 +28,7 @@ class Field extends Component {
               className={`form-control ${this.props.length === 'small' ? 'col-md-4' : 'col-md'}`}
               placeholder={this.props.placeholder}
               value={this.state.value}
+              disabled={this.props.disabled}
               onChange={event => this.handleChange(event)}/>
           </div>
         </div>
