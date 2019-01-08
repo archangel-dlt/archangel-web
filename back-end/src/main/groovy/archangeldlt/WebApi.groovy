fa8cc1223b763cdd3bf9e0fc6be287165fcaf360
Jez Higgins <jez@jezuk.co.uk>
2019-01-08 22:53:27 +0000

    Launch broser if on desktop

--- back-end/src/main/groovy/archangeldlt/WebApi.groovy
+++ back-end/src/main/groovy/archangeldlt/WebApi.groovy
@@ -4,9 +4,15 @@ import ratpack.groovy.GroovyRatpackMain
 
 import static archangeldlt.DroidWrapper.setupDroid
 
+import static java.awt.Desktop.isDesktopSupported
+import static java.awt.Desktop.getDesktop
+
 class WebApi extends GroovyRatpackMain {
   static void main(String... args) {
     setupDroid()
     GroovyRatpackMain.main(args)
+
+    if (isDesktopSupported())
+      getDesktop().browse(new URI('http://localhost:5050/'))
   }
 } // WebApi
